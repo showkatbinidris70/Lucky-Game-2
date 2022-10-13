1cb2c6d232ea78a8ee211cea29fdaad91f71c63c
showkatbinidris70 <showkatbinidris70@gmail.com>
2022-10-13 18:47:15 +0600

    lucky game 2

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const winGameElm = document.querySelector(".winGame");
+
 const luckyBtnElm = document.querySelector("#luckyBtn");
 const luckyNumberElm = document.querySelector(".lucky-number");
 const p1Elm = document.querySelector("#p1");
@@ -15,12 +17,28 @@ let p2luckyNumber = Math.floor(Math.random()*10)+1;
 luckyBtnElm.addEventListener('click', ()=>{
     luckyNumberElm.textContent = luckyNumber;
 });
-p1Elm.addEventListener('click',()=>{
+
+function p1LuckyNumbers(){
     p1LuckyNumberElm.textContent = p1luckyNumber;
-});
-p2Elm.addEventListener('click',()=>{
+    if(luckyNumber == p1luckyNumber){
+        winGameElm.textContent = "Player one is won the game";
+        p1Elm.setAttribute('disabled', 'disabled');
+        p2Elm.setAttribute('disabled', 'disabled');
+
+    }
+}
+p1Elm.addEventListener('click',p1LuckyNumbers);
+
+function p2LuckyNumbers(){
     p2LuckyNumberElm.textContent = p2luckyNumber;
-});
+    if(luckyNumber == p2luckyNumber){
+        winGameElm.textContent = "Player two is won the game";
+        p1Elm.setAttribute('disabled', 'disabled');
+        p2Elm.setAttribute('disabled', 'disabled');
+    }
+}
+p2Elm.addEventListener('click',p2LuckyNumbers);
+
 resetElm.addEventListener('click',()=>{
     luckyNumberElm.textContent = 0;
     p1LuckyNumberElm.textContent = 0;
